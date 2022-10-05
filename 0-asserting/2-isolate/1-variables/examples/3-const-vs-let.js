2c008d278a61a3e399352a757b3aa09a6336f3c0
BrianMunene96 <107561877+BrianMunene96@users.noreply.github.com>
2022-10-05 02:17:52 +0200

    inside js exercises, 0-asserting

--- 0-asserting/2-isolate/1-variables/examples/3-const-vs-let.js
+++ 0-asserting/2-isolate/1-variables/examples/3-const-vs-let.js
@@ -14,8 +14,8 @@ console.log('-- begin --');
 //  this is a "creation phase" error, meaning the code will not even execute!
 // uncomment the next two lines of code to get the error
 
-// const constantVariable1;
-// console.log(constantVariable1);
+ const constantVariable1;
+console.log(constantVariable1);
 
 // const variables also cannot be reassigned
 // this will throw an error
--- 0-asserting/2-isolate/2-comparing-and-asserting/examples/4-assertion-testing.js
+++ 0-asserting/2-isolate/2-comparing-and-asserting/examples/4-assertion-testing.js
@@ -11,20 +11,26 @@ console.log('-- begin --');
 // inline
 
 console.assert('hello' === 'hello', 'Inline 1');
+// ASSERTION is true so nothing happens
 
 console.assert('4' === 'four', 'Inline 2');
+//ASSERTION is falso so we see an error
 
 console.assert('good bye.' === 'good bye', 'Inline 3');
+// This assertion fails
 
 // with a variable (looks good in the debugger)
 
 const test1 = 'hello' === 'hello';
 console.assert(test1, 'With a Variable 1');
+// This assertion passes
 
 const test2 = '4' === 'four';
 console.assert(test2, 'With a Variable 2');
+// this one fails
 
 const test3 = 'good bye.' === 'good bye';
 console.assert(test3, 'With a Variable 3');
+// and so does this one
 
 console.log('-- end --');
