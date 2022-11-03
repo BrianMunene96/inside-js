a91e1451ee2f888fd718a67c468b4a61ba203952
BrianMunene96 <107561877+BrianMunene96@users.noreply.github.com>
2022-11-03 22:44:19 +0100

    Practice exercises of Unit Testing

--- 4-unit-testing/exercises/1-write-tests/add.test.js
+++ 4-unit-testing/exercises/1-write-tests/add.test.js
@@ -9,22 +9,24 @@
  * @param {number} [y=0] - The right number.
  * @returns {number} The sum of x and y.
  */
-__;
+const sumNumbers = (x, y) => {
+  return x + y;
+};
 
-describe('', () => {
-  describe('', () => {
-    it('', () => {
-      const expected = _;
-      const actual = _;
-      expect(_).toEqual(_);
+describe('Given two numbers we can find their sum using a function', () => {
+  describe('Find the sum of given numbers', () => {
+    it('Should retun 9 when 4 and 5 are parsed', () => {
+      const expected = sumNumbers(5, 4);
+      const actual = 9;
+      expect(expected).toEqual(actual);
     });
     // ...
   });
-  describe('', () => {
-    it('', () => {
-      const expected = _;
-      const actual = _;
-      expect(_).toEqual(_);
+  describe('The same should be true for negative numbers', () => {
+    it('Should retun 4 when 6 and -2 are passed', () => {
+      const expected = sumNumbers(6, -2);
+      const actual = 4;
+      expect(expected).toEqual(actual);
     });
     // ...
   });
